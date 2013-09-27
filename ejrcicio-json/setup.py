0395846e4113e69a24fcd30f40a36b3e1a0763e4
Gaston Severina <gseverina@gmail.com>
2013-09-27 20:47:59 -0300

    adding ejercicio

---
+++ ejrcicio-json/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ejercicio-json',
+    version='0.1.0',
+    author='Gaston Severina',
+    author_email='',
+    packages=['src','tests'],
+    url='',
+    license='GPL',
+    description='Una clase que representa una persona y se integra con JSON',
+)
+
---
+++ ejrcicio-json/tests/test0.py
@@ -0,0 +1,14 @@
+import unittest
+import src
+
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init_person(self)
+        p = src.Person.Person('Juan','Perez','1990-08-23')
+
+
+
+
